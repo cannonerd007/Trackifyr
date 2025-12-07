32f8b3121ccadeca8388e4d0640c34b6e1038ac2
dorodb-web22 <dorodbarma@gmail.com>
2025-12-07 09:37:37 +0530

    Add the openDB indexedDB helper

--- storage.js
+++ storage.js
@@ -10,3 +10,27 @@ const DB_NAME = 'ProjectTrackerDB';
 const DB_VERSION = 1;
 const STORE_NAME = 'tracker_store';
 
+/**
+ * @returns {Promise<IDBDatabase>}
+ */
+const openDB = () => {
+    return new Promise((resolve, reject) => {
+        const request = indexedDB.open(DB_NAME, DB_VERSION);
+
+        request.onupgradeneeded = (event) => {
+            const db = event.target.result;
+            if (!db.objectStoreNames.contains(STORE_NAME)) {
+                db.createObjectStore(STORE_NAME, { keyPath: 'key' });
+            }
+        };
+
+        request.onsuccess = (event) => {
+            resolve(event.target.result);
+        };
+
+        request.onerror = (event) => {
+            console.error("IndexedDB error:", event.target.error);
+            reject(event.target.error);
+        };
+    });
+};
