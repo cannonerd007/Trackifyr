59e6ac6d34c560fc44fabc8bd17e554c624c94c9
dorodb-web22 <dorodbarma@gmail.com>
2025-12-07 09:33:26 +0530

    storage keys and DB constants

---
+++ storage.js
@@ -0,0 +1,12 @@
+
+
+export const STORAGE_KEYS = {
+    PROJECTS: 'projectTracker_projects',
+    ACTIVE_PROJECT_ID: 'projectTracker_activeProjectId',
+    THEME: 'projectTracker_theme'
+};
+
+const DB_NAME = 'ProjectTrackerDB';
+const DB_VERSION = 1;
+const STORE_NAME = 'tracker_store';
+
